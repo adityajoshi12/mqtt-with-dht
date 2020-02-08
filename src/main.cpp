6e9f3e989a29da2debc7e5b49aaa25636c8b76ee
Aditya Joshi <aditya@interwork.biz>
2020-02-08 22:14:28 +0530

    mqtt with DHT11

---
+++ src/main.cpp
@@ -0,0 +1,154 @@
+#include <Arduino.h>
+#include "DHT.h"
+#include <Adafruit_Sensor.h>
+#include <DHT_U.h>
+#include <string.h>
+#include <ESP8266WiFi.h>
+#include <FS.h>
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+
+#define DHTTYPE DHT11
+#define DHT11_PIN 2
+uint32_t delayMS = 3000;
+
+DHT dht(DHT11_PIN, DHTTYPE);
+
+char tempString[50];
+char humidityString[50];
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char *ssid = "Honor 9 Lite_658E";
+const char *password = "tanuja276";
+const char *mqtt_server = "192.168.43.209";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup_wifi()
+{
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1')
+  {
+    digitalWrite(BUILTIN_LED, LOW); // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  }
+  else
+  {
+    digitalWrite(BUILTIN_LED, HIGH); // Turn the LED off by making the voltage HIGH
+  }
+}
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup()
+{
+  pinMode(BUILTIN_LED, OUTPUT); // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(9600);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop(){
+  delay(delayMS);
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  double f = dht.readTemperature(true);
+  if (isnan(h) || isnan(t) || isnan(f))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  dtostrf(t, 6, 2, tempString);
+  client.publish("temp", tempString);
+  dtostrf(h, 6, 4, humidityString);
+  client.publish("humidity", humidityString);
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+  delay(delayMS);
+}
